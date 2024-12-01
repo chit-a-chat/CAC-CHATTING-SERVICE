ccf085c2e1b042fff766a9858ea77515a668cf71
nowgnas <dev.nowgnas@gmail.com>
2024-12-01 21:02:31 +0900

    [chore]: project initial commit

---
+++ src/main/java/com/chitachat/chatting/ChattingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chitachat.chatting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChattingServiceApplication.class, args);
+    }
+
+}
